a0f1fa4af266e7ce4c68a9af3111652a760a6931
ziyuansun <ziyuans@umich.edu>
2018-08-24 11:42:00 -0400

    first commit

---
+++ projecteuler 20-25.py
@@ -0,0 +1,64 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+5/2
+
+
+# In[2]:
+
+
+6//3
+
+
+# In[3]:
+
+
+7//3
+
+
+# In[4]:
+
+
+a=[1,2,3,4]
+sum(a)
+
+
+# In[5]:
+
+
+
+f=1
+for i in range(2,101):
+    f=f*ile
+
+sum_=0
+while f>0:
+    sum_=sum_+f%10
+    f=f//10
+print(sum_)
+
+
+# In[9]:
+
+
+def mysum(list_):
+    import functools
+    return functools.reduce(lambda x,y:x+y, list_)
+f=1
+for i in range(2,101):
+    f=f*i
+sum_=0
+a=[int(c) for c in str(f)]
+print(mysum(a))
+
+
+# In[7]:
+
+
+def mysum(list_):
+    import functools
+    return functools.reduce(lambda x,y:x+y, list_)
+
